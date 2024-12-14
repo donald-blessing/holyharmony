560fe3e0946df46e05a1341062e78deaaf4328aa
Donald Blessing <donaldblessing9@gmail.com>
2024-12-14 07:39:34 +0100

    Rector config

--- rector.php
+++ rector.php
@@ -39,20 +39,19 @@ use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/app',
-        __DIR__ . '/database',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
+        __DIR__.'/app',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
-    ->withSkipPath(__DIR__ . '/app/Providers')
-    ->withSkipPath(__DIR__ . '/database/migrations')
+    ->withSkipPath(__DIR__.'/app/Providers')
+    ->withSkipPath(__DIR__.'/database/migrations')
     ->withSets([
         LaravelLevelSetList::UP_TO_LARAVEL_110,
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelSetList::LARAVEL_COLLECTION,
         LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
         LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
-        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
         LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
         LaravelSetList::LARAVEL_IF_HELPERS,
         //        LaravelSetList::LARAVEL_STATIC_TO_INJECTION,
